6e67dfc6c226d571fd104634661f25eff49f2dde
Girish Patwardhan <reach.to.girish@gmail.com>
2018-07-31 14:01:39 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,8 +3,9 @@ console.log('Loaded service worker!');
 self.addEventListener('push', ev => {
   const data = ev.data.json();
   console.log('Got push', data);
-  self.registration.showNotification(data.title, {
+  /*self.registration.showNotification(data.title, {
     body: 'Hello, World GP!',
     icon: 'http://mongoosejs.com/docs/images/mongoose5_62x30_transparent.png'
-  });
+  });*/
+  alert(data.title);
 });
