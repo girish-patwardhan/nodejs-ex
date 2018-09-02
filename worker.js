7cfb62ad1285ba2a3ca913f2bf37b5239d5143b4
Girish Patwardhan <reach.to.girish@gmail.com>
2018-09-02 01:34:52 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -17,3 +17,26 @@ function createGist(opts) {
     console.log('Sent Notification , response json is :', data);
   });
 }
+
+function createGistGET(opts) {
+
+  console.log('Posting request to GitHub API...');
+  fetch('http://localhost:4500/notify?a=23')
+.then(response => response.json())
+.then(data => {
+  console.log(data) // Prints result from `response.json()` in getRequest
+})
+.catch(error => console.error(error))
+}
+
+function postRequest(url, data) {
+  return fetch(url, {
+    credentials: 'omit', // 'include', default: 'omit'
+    method: 'POST', // 'GET', 'PUT', 'DELETE', etc.
+    body: JSON.stringify(data), // Coordinate the body type with 'Content-Type'
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    }),
+  })
+  .then(response => response.json())
+}
